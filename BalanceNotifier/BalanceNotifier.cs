7c1bd49c6e0bd1c5976d07044a5d75301c025973
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-04-18 01:42:15 -0400

    experiment: Change to every minute to test function app.

--- BalanceNotifier/BalanceNotifier.cs
+++ BalanceNotifier/BalanceNotifier.cs
@@ -20,7 +20,7 @@ public class BalanceNotifier
     /// existing TODO to fix this at TODO[1] in main.tf.
     /// </para>
     /// </summary>
-    private const string EverydayAt8AmEt = "0 0 12 * * *";
+    private const string EveryMinute = "0 * * * * *";
 
     /// <summary>
     /// A service used for getting banking information via a banking API.
@@ -50,7 +50,7 @@ public class BalanceNotifier
     /// function is triggered.</param>
     /// <param name="logger">A logger object for logging information.</param>
     [FunctionName(nameof(BalanceNotifier))]
-    public async Task Run([TimerTrigger(EverydayAt8AmEt)] TimerInfo timerInfo, ILogger logger)
+    public async Task Run([TimerTrigger(EveryMinute)] TimerInfo timerInfo, ILogger logger)
     {
         logger.LogInformation($"Timer trigger function started execution at: {DateTime.UtcNow}.");
 
