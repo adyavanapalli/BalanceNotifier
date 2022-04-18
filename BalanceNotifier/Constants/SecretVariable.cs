c2ad13bd87d5ff1ac281fc5a01dbb693611cf3dd
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-04-18 00:39:01 -0400

    fix: Use `([A-Z]-?)+` naming convention for secrets in Key Vault, as required.

--- BalanceNotifier/Constants/SecretVariable.cs
+++ BalanceNotifier/Constants/SecretVariable.cs
@@ -8,35 +8,35 @@ public class SecretVariable
     /// <summary>
     /// The name of the secret variable representing a Plaid client ID.
     /// </summary>
-    public const string PlaidClientId = "PLAID_CLIENT_ID";
+    public const string PlaidClientId = "PLAID-CLIENT-ID";
 
     /// <summary>
     /// The name of the secret variable representing a Plaid client secret.
     /// </summary>
-    public const string PlaidClientSecret = "PLAID_CLIENT_SECRET";
+    public const string PlaidClientSecret = "PLAID-CLIENT-SECRET";
 
     /// <summary>
     /// The name of the secret variable representing a Plaid client access token.
     /// </summary>
-    public const string PlaidClientAccessToken = "PLAID_CLIENT_ACCESS_TOKEN";
+    public const string PlaidClientAccessToken = "PLAID-CLIENT-ACCESS-TOKEN";
 
     /// <summary>
     /// The name of the secret variable representing a Twilio account SID.
     /// </summary>
-    public const string TwilioAccountSid = "TWILIO_ACCOUNT_SID";
+    public const string TwilioAccountSid = "TWILIO-ACCOUNT-SID";
 
     /// <summary>
     /// The name of the secret variable representing a Twilio authentication token.
     /// </summary>
-    public const string TwilioAuthenticationToken = "TWILIO_AUTHENTICATION_TOKEN";
+    public const string TwilioAuthenticationToken = "TWILIO-AUTHENTICATION-TOKEN";
 
     /// <summary>
     /// The name of the secret variable representing the Twilio sender phone number.
     /// </summary>
-    public const string TwilioSenderPhoneNumber = "TWILIO_SENDER_PHONE_NUMBER";
+    public const string TwilioSenderPhoneNumber = "TWILIO-SENDER-PHONE-NUMBER";
 
     /// <summary>
     /// The name of the secret variable representing the Twilio recipient phone number.
     /// </summary>
-    public const string TwilioRecipientPhoneNumber = "TWILIO_RECIPIENT_PHONE_NUMBER";
+    public const string TwilioRecipientPhoneNumber = "TWILIO-RECIPIENT-PHONE-NUMBER";
 }
